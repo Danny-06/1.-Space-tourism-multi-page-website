f8995b16b85c73083c8828a401e99883546893ee
“Daniel” <64810647+Xion-14@users.noreply.github.com>
2023-01-18 14:48:59 +0100

    Fix title-page

--- src/components/title-page/title-page.js
+++ src/components/title-page/title-page.js
@@ -2,7 +2,7 @@ import _ from "../../libs/functional-dom/index.js";
 
 export default function TitlePage(index, name) {
   return _.div({class: 'title-page'},
-    _.div({class: 'index'}, '02'),
-    _.div({class: 'name'}, 'MEET YOUR CREW'),
+    _.div({class: 'index'}, index),
+    _.div({class: 'name'}, name),
   )
 }
